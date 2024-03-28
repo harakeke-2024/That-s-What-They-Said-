2107015e6e7b2dc31a103d2638eab2f9a21e7053
Connor Wiltshire <cwnz2004@gmail.com>
2024-03-29 07:52:34 +1300

    Completed Homepage and Questions

---
+++ client/components/Homepage.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+export default function Homepage() {
+  return (
+    <>
+      <div className="container">
+        <div id="content_container">
+          <div id="content">
+            <h3>Dev Academy</h3>
+            <h1>Trivia Night</h1>
+            <h4>Powered by Deez Nuts</h4>
+            <ul>
+              <li>
+                <button className="button_1">
+                  <Link className="white_link" to={`/Questions`}>
+                    Sign in
+                  </Link>
+                </button>
+              </li>
+              <li>
+                <button className="button_2">Play as Guest</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
