f7558cf2035cbe0f61245557a6de0682b45742e3
Sonia <155363911+sonia-huynh@users.noreply.github.com>
2024-03-29 14:11:10 +1300

    Auth0-sign in takes to next page

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { useAuth0 } from '@auth0/auth0-react'
+import Questions from './Questions.tsx'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   const { user, logout, loginWithRedirect } = useAuth0()
@@ -17,17 +20,26 @@ function Nav() {
 
   return (
     <>
-      <IfAuthenticated>
-        <button className="button_1 white_link" onClick={handleSignOut}>
-          Sign out
-        </button>
-        {user && <p>Signed in as: {user?.nickname}</p>}
-      </IfAuthenticated>
-      <IfNotAuthenticated>
-        <button className="button_1 white_link" onClick={handleSignIn}>
-          Sign in
-        </button>
-      </IfNotAuthenticated>
+      <div className="container">
+        <div id="content_container">
+          <div id="content">
+            <h3>Dev Academy</h3>
+            <h1>Trivia Night</h1>
+            <h4>Powered by Deez Nuts</h4>
+            <IfNotAuthenticated>
+              <button className="button_1 white_link" onClick={handleSignIn}>
+                Sign in
+              </button>
+            </IfNotAuthenticated>
+            <IfAuthenticated>
+              <button className="button_1 white_link" onClick={handleSignOut}>
+                Sign out
+              </button>
+              {user && <p>Signed in as: {user?.nickname}</p>}
+            </IfAuthenticated>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
